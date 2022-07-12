eec9321184a8de3cf520cb19d7f520391bc42143
DESKTOP-PB7NIIJ\hna20 <hna206@naver.com>
2022-07-13 00:25:32 +1200

    Basic Setting from Empty to MVC framework

--- DotNetNote/Startup.cs
+++ DotNetNote/Startup.cs
@@ -16,6 +16,7 @@ namespace DotNetNote
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews(); //To use MVC Framework, need to apply this middleware. --1
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,6 +32,7 @@ namespace DotNetNote
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapDefaultControllerRoute(); // To use MVC Framework --2
                 endpoints.MapGet("/", async context =>
                 {
                     //await context.Response.WriteAsync("Hello World!");
