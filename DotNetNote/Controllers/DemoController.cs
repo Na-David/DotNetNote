1b51385c36c110b86da7683bfef0d29a73f7b586
DESKTOP-PB7NIIJ\hna20 <hna206@naver.com>
2022-07-13 02:01:15 +1200

    Model, View, Controller

---
+++ DotNetNote/Controllers/DemoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetNote.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index() // when we try to access to index, /Demo/Index
+        {
+            //return Content("DEMO Page");
+            return View();
+        }
+    }
+}
